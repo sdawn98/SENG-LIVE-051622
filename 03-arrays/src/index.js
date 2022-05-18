371e0fa5b75f4b6368e91441281d30ce3d6b994a
ixnp <enprocopios@gmail.com>
2022-05-18 07:00:29 -0700

    starter

---
+++ 03-arrays/src/index.js
@@ -0,0 +1,10 @@
+// 1. Store the books and customer names in arrays. 
+
+// 2. Write a functions that can transform book elements. 
+//     - Change books to uppercase 
+//     - Remove the author 
+//     - remove the title.
+
+// 3. Write a function that will filter books out by a search term.
+
+// 4. Write a function that returns a specific book by search term. 
